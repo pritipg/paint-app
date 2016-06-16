a85f0c878174188736c2252268a85a09aa5ddd5f
Priti <pripra3014@gmail.com>
2016-06-16 11:42:09 +0530

    finding relative position

--- js/paint.js
+++ js/paint.js
@@ -44,5 +44,12 @@ controls.select = function(cx) {
   return elt("span", null, "Tools: ", select);
 };
 
+function relativePos(event, element) {
+  var rect = element.getBoundingClientRect();
+  return {x: Math.floor(event.clientX - rect.left),
+          y: Math.floor(event.clientY - rect.top)};
+}
+
+
 var app = document.querySelector("#app");
 createPaint(app);
